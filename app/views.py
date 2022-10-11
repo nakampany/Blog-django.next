04de062c560ce446f96288688f0d75d1861841b6
naka <nakasyo2792@gmail.com>
2022-10-11 23:40:38 +0900

    backend

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PostSerializer
+from .models import Post
+
+
+class PostView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
